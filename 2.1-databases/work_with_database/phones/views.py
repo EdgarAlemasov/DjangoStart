17130fac9020a2300feca75b69c35b0217e01577
Edgar Alemasov <edgar.alemasov@gmail.com>
2022-02-25 00:07:11 +0500

    sorted phones

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 
+from phones.models import Phone
+
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +9,24 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort_pages = request.GET.get('sort')
+    phones = Phone.objects.all()
+    if sort_pages == 'min_price':
+        phones = phones.order_by('price')
+    elif sort_pages == 'max_price':
+        phones = phones.order_by('-price')
+    elif sort_pages == 'name':
+        phones = phones.order_by('name')
+    context = {
+        'phones': phones,
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.get(slug__contains=slug)
+    context = {
+        'phone': phone,
+    }
     return render(request, template, context)
